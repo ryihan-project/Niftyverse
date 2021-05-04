8cf12c3ad7895deee0b98759de453f5e37c14532
raihan <ryihan55@gmail.com>
2021-05-05 00:34:01 +0600

    Update App.js

--- App.js
+++ App.js
@@ -5,12 +5,14 @@ import {StyleSheet,Text,View} from 'react-native';
 import logo from './assets/Logo/Logo-72_x_72.png'; 
 //Header Import
 import Header from './Component/Header';
+import Feed from './Screen/Feed';
 
 export default function App() {
   return (
     <View style={styles.container}>
         
       <Header/>
+      <Feed/>
     </View>
   );
 }
