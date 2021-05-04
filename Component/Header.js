a6ed530768f1030b845251cca698553b35c1bcfd
raihan <ryihan55@gmail.com>
2021-05-05 00:16:45 +0600

    Update Header.js

--- Component/Header.js
+++ Component/Header.js
@@ -26,6 +26,7 @@ Header:{
 		//alignItems:  'center',
 		//justifyContent: 'center',
 		borderRadius: 20,
+		shadowColor: '#000',
 	    shadowOffset: { width: 0, height: 1 },
 	    shadowOpacity: 0.8,
 	    shadowRadius: 2,  
