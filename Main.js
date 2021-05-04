4fa702bd3aa94d5bc3364b6af06c4fb334918bef
raihan <ryihan55@gmail.com>
2021-05-05 05:02:56 +0600

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,3 +1,13 @@
+export default function App() {
+  return (
+    <View style={styles.container}>
+        
+      <Header/>
+      <Feed/>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
  container:{
   flex: 1,
