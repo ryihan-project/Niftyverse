5dbeaf2f404ecd6846c35f80edfe12d36768e660
raihan <ryihan55@gmail.com>
2021-05-05 05:03:00 +0600

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,3 +1,12 @@
+//import { StatusBar } from 'expo-status-bar';
+//import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import {StyleSheet,Text,View} from 'react-native';
+import logo from './assets/Logo/Logo-72_x_72.png'; 
+//Header Import
+import Header from './Component/Header';
+import Feed from './Screen/Feed';
+
 export default function App() {
   return (
     <View style={styles.container}>
