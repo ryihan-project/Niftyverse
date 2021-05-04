21e2282c0f15f272e1511b65b06b290b1b80e8ba
raihan <ryihan55@gmail.com>
2021-05-04 21:52:28 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
+//import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet,Text,View} from 'react-native';
+import logo from './assets/Logo/Logo-72_x_72.png'; 
+//Header Import
+import Header from './Component/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title='Raihan'>
+        
+      </Header>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container:{
+  flex: 1,
+ },
 });
