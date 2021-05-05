d4eaf257193aa5101761350b08c58067cf0b520e
raihan <ryihan55@gmail.com>
2021-05-05 13:41:37 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 //import { StatusBar } from 'expo-status-bar';
 //import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
+import {StyleSheet,Text,View,ScrollView} from 'react-native';
 import logo from './assets/Logo/Logo-72_x_72.png'; 
 //Header Import
 import Header from './Component/Header';
