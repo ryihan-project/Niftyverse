c078ff1a559a0fdb8d4169e523aafb9f96a9da96
raihan <ryihan55@gmail.com>
2021-05-05 13:44:45 +0600

    Create Log.js

---
+++ Log.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { 
+  StyleSheet,
+  Text,
+  View ,
+  Image ,
+  Button,
+  TextInput,
+  TouchableHighlight
+   } from 'react-native';
